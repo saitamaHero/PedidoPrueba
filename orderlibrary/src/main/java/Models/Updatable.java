17d9a550ccc0d6e772951482a45fb59c53b6966b
Dionicio Acevedo <tec.dionicioacevedo@gmail.com>
2018-11-27 16:04:59 -0400

    Documentation added

--- orderlibrary/src/main/java/Models/Updatable.java
+++ orderlibrary/src/main/java/Models/Updatable.java
@@ -1,5 +1,10 @@
 package Models;
 
 public interface Updatable<T> {
+    /**
+     * Actualiza un objeto con otro objeto del tipo deseado
+     * @param item
+     * @return verdadero si el elemento ha sido actualizado correctamente, falso en caso contrario.
+     */
     public boolean update(T item);
 }
